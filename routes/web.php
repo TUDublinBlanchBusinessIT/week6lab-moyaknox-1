8da2c9b1a308fc5a2ce3cdf5a64e7ab60fc7e652
B00153832 Moya Knox <B00153832@mytudublin.ie>
2025-03-03 16:05:14 +0000

    add to cart working

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,7 @@ Route::get('/', function () {
 
 Route::get('product/displaygrid', 'App\Http\Controllers\productController@displaygrid')->name('products.displaygrid');
 
+Route::get('product/additem/{id}', 'App\Http\Controllers\productController@additem')->name('products.additem');
 
 Route::resource('products', App\Http\Controllers\productController::class);
 
@@ -27,3 +28,4 @@ Route::resource('orderdetails', App\Http\Controllers\orderdetailController::clas
 
 
 Route::resource('scorders', App\Http\Controllers\scorderController::class);
+
